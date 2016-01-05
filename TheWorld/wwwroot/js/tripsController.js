6fa679f2ce1f06fb193e7c42ea2e68fe01077576
Charlie Hager <charlie80088@gmail.com>
2016-01-05 13:27:16 -0700

    Code up to, but not including, angular routing

---
+++ TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    angular.module("app-trips")
+    .controller("tripsController", tripsController);
+
+    function tripsController($http) {
+        //when this function is called it returns and object (as in: f = new tripsController()) this == f (the new object)
+        var vm = this;
+
+        vm.trips = [];
+
+        vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                //success
+                angular.copy(response.data, vm.trips);
+            },
+             function (error) {
+                 //failure
+                 vm.errorMessage = "Failed to load data: " + error.statusText;
+             })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+        vm.addTrip = function () {
+            vm.isBusy = true;
+            vm.errorMessage = '';
+
+            $http.post("/api/trips", vm.newTrip)
+            .then(function (response) {
+                //success
+                vm.trips.push(response.data);
+                vm.newTrip = {};
+            },
+             function (error) {
+                 //failure
+                 vm.errorMessage = "Failed to save new trip, try again.";
+             })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+
+        };
+    };
+})();
